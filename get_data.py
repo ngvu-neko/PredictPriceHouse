3bdfa905482e51df757199be5cc2cfa7dc32a412
ngvu-neko <79347610+ngvu-neko@users.noreply.github.com>
2021-09-19 15:37:06 +0900

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 
-chrome_diver_path = "../chromedriver_win32/chromedriver"
+chrome_diver_path = "...."
 driver = webdriver.Chrome(executable_path= chrome_diver_path)
-driver.get("https://suumo.jp/jj/bukken/ichiran/JJ010FJ001/?ar=030&bs=020&ta=13&jspIdFlg=patternShikugun&sc=13103&sc=13109&sc=13111&kb=1&kt=9999999&km=1&tb=0&tt=9999999&hb=0&ht=9999999&ekTjCd=&ekTjNm=&tj=0&kw=1&srch_navi=1&pn=2")
+driver.get("webname")
 
 #find price of house
 price_list = driver.find_elements_by_class_name('dottable-value')
